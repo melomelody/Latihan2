9444ea37c2441e6985da420599b440531b78fafb
melomelody <melomelody.gm@gmail.com>
2018-01-04 17:00:15 +0700

    Lanjutan

---
+++ SoalC.java
@@ -0,0 +1,17 @@
+public class SoalC{
+    public static void main(String[] args){
+        int i,j,k;
+        k=6;
+        for(i = 0; i <= 3 ; i++){
+            for(j=(0+k); j<7; j++){
+                System.out.print("*");
+      
+            }
+            System.out.println(" ");
+        k--;
+        k--;    
+        }
+    
+    }
+   
+}
---
+++ SoalE.java
@@ -0,0 +1,12 @@
+public class SoalE{
+    public static void main(String[] args){
+        int i,j;
+        for(i = 1; i <=5 ; i++){
+            for(j=5; j >=i; j--){
+                System.out.print(j);
+                
+            }
+            System.out.println(" ");
+        }
+    }
+}
