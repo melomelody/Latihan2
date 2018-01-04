3602dc8c096d60d839e7d56d7b52e59f84d03ce8
melomelody <melomelody.gm@gmail.com>
2018-01-04 14:08:10 +0700

    Soal Bilangan genap

---
+++ SoalA.java
@@ -0,0 +1,13 @@
+public class SoalA{
+    public static void main (String[] args){
+
+        Integer bil;
+
+        for(bil=1; bil < 100; bil++){
+            if(bil % 2 == 0){
+            System.out.print(bil+","+" ");
+            }
+            
+        }
+    }
+}
